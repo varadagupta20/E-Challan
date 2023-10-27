f1c7e4a45b35b1c135e824fade6d8b2e42c17598
Varada Gupta <143926527+varadagupta20@users.noreply.github.com>
2023-10-27 16:14:57 +0530

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define('DBHOST', 'localhost');
-	define('DBUSER', 'root');
-	define('DBPASS', '');
-	define('DBNAME', 'traffic');
-	
-	$conn = mysqli_connect(DBHOST,DBUSER,DBPASS, DBNAME) or die("Connection failed : ")
-?>
